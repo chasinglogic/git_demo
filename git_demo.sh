9fbf35b86cf2e6b802236584fc954e2c0004cb4b
Mathew Robinson <chasinglogic@gmail.com>
2017-05-08 09:25:51 -0400

    almost completed demo

--- git_demo.sh
+++ git_demo.sh
@@ -1,22 +1,78 @@
 #!/bin/bash
 
-COMMANDS=(
-    ""
-)
+# Repeats a character x times
+printx_times() {
+    str=$1
+    num=$2
+    v=$(printf "%-${num}s" "$str")
+    echo "${v// /$str}"
+}
+
+# Used for alignment
+COLS=80
+HALF_COLS=40
 
 REPO="$(PWD)"
 cd ..
-OTHER_REPO="$(PWD)/${REPO}_other"
+OTHER_REPO="$(PWD)/git_demo_other"
+
+if [ -d $OTHER_REPO ]; then
+    rm -rf $OTHER_REPO
+fi
+
+echo "Copying ${REPO} to ${OTHER_REPO}"
 
 cp -R $REPO $OTHER_REPO
 cp -R $REPO/.git $OTHER_REPO/
 
+cd $OTHER_REPO
 
-for COMMAND in "$COMMANDS"; do
-    git status
-    echo "================================================================================"
-    printf "%45s\n" "$COMMAND"
-    echo "================================================================================"
-    $COMMAND
+git checkout -b demo
+printf "\nREMOTE CHANGES" >> test.txt
+git add --all
+git commit -m "make a change in text.txt to cause a conflict"
+git push origin demo
+
+cd $REPO
+
+git checkout -b demo
+
+clear
+
+COMMANDS=(
+    "git status"
+    "git log"
+    "git branch -v"
+    "git checkout -b make-some-changes"
+    "git branch -v"
+    "echo 'test' > new.txt"
+    "git status"
+    "git add new.txt"
+    "git status"
+    "git commit -m 'add new.txt'"
+    "git status"
+    "echo 'local changes' >> test.txt"
+    "git add test.txt"
+    "git commit -m 'make some local changes'"
+    "git status"
+    "git checkout demo"
+    "git pull origin demo"
+    "git checkout make-some-changes"
+    "git log --graph --full-history --all --color --pretty=format:\"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s\""
+)
+
+for COMMAND in "${COMMANDS[@]}"; do
+    printx_times "=" $COLS
+    printf "%*s\n" $(( (${#COMMAND} + $COLS ) / 2)) "$COMMAND"
+    printx_times "=" $COLS
+    /bin/bash -c "$COMMAND"
     read  -n 1
+    clear
 done
+
+# Cleanup
+git checkout master > /dev/null &2>/dev/null
+git push origin --delete make-some-changes > /dev/null &2>/dev/null
+git branch -d make-some-changes > /dev/null &2>/dev/null
+git push origin --delete demo > /dev/null &2>/dev/null
+git branch -d demo > /dev/null &2>/dev/null
