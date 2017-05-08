5047966e6cc44acb549b3012b32584c5bb505e9b
Mathew Robinson <chasinglogic@gmail.com>
2017-05-08 10:04:23 -0400

    add an 'end slide'

--- git_demo.sh
+++ git_demo.sh
@@ -67,6 +67,7 @@ COMMANDS=(
     "git push origin demo"
     "git log --graph --full-history --all --color --pretty=format:\"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s\""
     "git status"
+    "THE END"
 )
 
 git checkout -b demo
